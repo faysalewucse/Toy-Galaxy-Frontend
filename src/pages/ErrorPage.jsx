d6ac8133d82bdcbf07a95b7af8d6416fe058ec24
faysalewucse <faysal.ewucse@gmail.com>
2023-05-21 07:05:04 +0600

    Fixed ErrorPage Design

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -6,9 +6,9 @@ export default function ErrorPage() {
   const navigate = useNavigate();
   return (
     <div>
-      <div className="max-w-7xl mx-auto">
-        <img className="w-3/4 mx-auto" src={errorImage} alt="error" />
-        <div className="text-center md:absolute md:top-20 md:left-1/2 md:transform md:-translate-x-1/2">
+      <div className="max-w-7xl mx-auto p-10">
+        <img className="md:w-3/4 mx-auto" src={errorImage} alt="error" />
+        <div className="text-center md:absolute md:top-40 md:left-1/2 md:transform md:-translate-x-1/2">
           <h1 className="text-8xl md:text-9xl font-bold text-blue-200">404</h1>
           <h1 className="my-2 text-gray-800 font-bold text-3xl md:text-5xl">
             Page not found!
@@ -19,7 +19,9 @@ export default function ErrorPage() {
         </div>
         <PrimaryButton
           text={"Take me to Home Page!"}
-          style={"md:w-1/3 w-3/4 mx-auto"}
+          style={
+            "md:w-1/3 w-3/4 mx-auto md:absolute md:bottom-20 md:left-1/2 md:transform md:-translate-x-1/2"
+          }
           onClickHandler={() => navigate("/")}
         />
       </div>
